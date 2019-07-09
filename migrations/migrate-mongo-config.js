f08b91ae9c59f9628975c4d456c7dba3f09872a3
Evgeny Mazurov <deivoff@gmail.com>
2019-07-09 19:18:06 +0500

    : add : add cobfig for docker

--- migrations/migrate-mongo-config.js
+++ migrations/migrate-mongo-config.js
@@ -4,10 +4,8 @@ require('dotenv').config({ path: path.join(__dirname + './../.env') });
 
 const config = {
   mongodb: {
-    // TODO Change (or review) the url to your MongoDB:
     url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@opeb-cities-ffxwj.mongodb.net/myos?retryWrites=true&w=majority`,
 
-    // TODO Change this to your database name:
     databaseName: 'myos',
 
     options: {
@@ -17,10 +15,7 @@ const config = {
     },
   },
 
-  // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
   migrationsDir: 'migrations',
-
-  // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
   changelogCollectionName: 'changelog',
 };
 
