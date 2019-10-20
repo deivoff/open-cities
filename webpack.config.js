3237270d201132c2d91be85c8dda8ceba7b5c26f
Eugeny Mazurov <deivoff@gmail.com>
2019-10-20 11:52:52 +0300

    : add : fix build bugs

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      parallel: true,
+      terserOptions: {
+        keep_classnames: true,
+	      keep_fnames: true,
+        output: {
+          comments: false,
+        },
+      },
+      extractComments: false,
+    })],
+  },
+  module: {
+    rules: [
+      {
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'frontend'),
+        ],
+        test: /\.ts$/,
+        use: 'ts-loader'
+      },
+    ]
+  },
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.gql', '.graphql']
+  },
+  target: 'node',
+  devtool: 'source-map',
+  entry: [path.join(__dirname, 'server/index.ts')],
+  externals: [nodeExternals({})],
+  mode: 'production',
+  plugins: [new CleanWebpackPlugin()]
+};
