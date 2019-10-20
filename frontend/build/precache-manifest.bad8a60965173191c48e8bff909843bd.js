2fa1eda5a535bd6804a9b7d8f809fb75fa8b0877
Eugeny Mazurov <deivoff@gmail.com>
2019-10-20 18:37:10 +0300

    : add : add build in production

---
+++ frontend/build/precache-manifest.bad8a60965173191c48e8bff909843bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2e075d8605c46153944606cb3a1806",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1abc7093208a6bb787f",
+    "url": "/static/css/main.a24c7565.chunk.css"
+  },
+  {
+    "revision": "349dee507c3ca4defa26",
+    "url": "/static/js/2.4bfe9f50.chunk.js"
+  },
+  {
+    "revision": "a1abc7093208a6bb787f",
+    "url": "/static/js/main.80798b8f.chunk.js"
+  },
+  {
+    "revision": "219707186523ad4b8cde",
+    "url": "/static/js/runtime~main.821e3928.js"
+  }
+]);
